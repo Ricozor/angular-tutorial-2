7d53b746db66d9604a34747b77a26950bb2467ed
Benoit Brochier <bbrochier.ext@orange.com>
2017-07-10 18:04:48 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None //'Emulated'(default), 'Native'
+})
+export class AppComponent {
+  serverElements = [{type: 'server', name:'testserver', content:'this is a test'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+}
